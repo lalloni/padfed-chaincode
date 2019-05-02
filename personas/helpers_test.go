ac38e54faf240bccc23271cdb103a34faf3053ea
Pablo Lalloni <plalloni@gmail.com>
2019-05-02 12:08:29 -0300

    Elimina test obsoleto

--- personas/helpers_test.go
+++ personas/helpers_test.go
@@ -1,11 +1,8 @@
 package personas_test
 
 import (
-	"encoding/json"
-	"strconv"
 	"testing"
 
-	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/inscripciones"
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/model"
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/personas"
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/test"
@@ -22,23 +19,5 @@ func TestValidPersona(t *testing.T) {
 	}
 	if err := model.ArgToPersona([]byte("{error-dummy"), &persona); err.IsOK() {
 		t.Error("JSON invalido, debe dar error " + err.Msg)
-	}
-}
-
-func TestValimpuestosJSON(t *testing.T) {
-	const cuit = 30679638943
-	var per model.Persona
-	err := json.Unmarshal(test.GetPersonaJSON(cuit), &per)
-	if err != nil {
-		t.Error("Error Failed to decode JSON of Impuestos")
-	}
-	if len(per.Impuestos) != 4 {
-		t.Errorf("Persona debe tener 4 impuestos y tiene %d", len(per.Impuestos))
-	}
-	for _, imp := range per.Impuestos {
-		v := inscripciones.GetImpuestoKeyByCuitID(cuit, imp.Impuesto)
-		if v != "PER_"+strconv.FormatUint(cuit, 10)+"_IMP_"+strconv.FormatUint(uint64(imp.Impuesto), 10) {
-			t.Errorf("1-Impuesto.Key no valido: %v", v)
-		}
 	}
 }
