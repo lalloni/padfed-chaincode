d2b38d80680b4cefc526f3589259ecdf9662b6a5
Pablo Lalloni <plalloni@gmail.com>
2019-05-08 09:44:29 -0300

    Elimina código obsoleto y mueve test de validación al paquete legítimo

--- model/validation_test.go
+++ model/validation_test.go
@@ -1,10 +1,9 @@
-package personas_test
+package model_test
 
 import (
 	"testing"
 
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/model"
-	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/personas"
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/test"
 )
 
@@ -13,9 +12,6 @@ func TestValidPersona(t *testing.T) {
 	var personaJSON = test.GetPersonaJSON(30679638943)
 	if err := model.ArgToPersona(personaJSON, &persona); !err.IsOK() {
 		t.Error(err.Msg)
-	}
-	if personas.GetPersonaKey(&persona) != "PER_30679638943" {
-		t.Error("Persona.Key no valida " + personas.GetPersonaKey(&persona))
 	}
 	if err := model.ArgToPersona([]byte("{error-dummy"), &persona); err.IsOK() {
 		t.Error("JSON invalido, debe dar error " + err.Msg)
