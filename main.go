b1cf9e7ce1465dbd2cc7aabcb7580942f8d54320
Pablo Lalloni <plalloni@gmail.com>
2019-07-02 16:40:41 -0300

    Actualiza main.go en función de últimos cambios
    Define handlers en función de nuevas implementaciones de handlers.

--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/deprecated"
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/deprecated/fabric"
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/deprecated/personas"
-	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/handlers/generic"
+	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/handlers/common"
+	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/handlers/impuesto"
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/handlers/persona"
-	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/ng/authorization"
+	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/handlers/state"
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/ng/chaincode"
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/ng/handler"
 	"gitlab.cloudint.afip.gob.ar/blockchain-team/padfed-chaincode.git/ng/logging"
@@ -19,39 +20,23 @@ const name = "padfedcc"
 
 var log = logging.Setup(name)
 
-var (
-	OnlyAFIP = authorization.MSPID("AFIP")
-	Free     = authorization.Allowed
-)
-
 func main() {
 
 	r := router.New()
 
-	r.SetInitHandler(OnlyAFIP, handler.SuccessHandler)
+	r.SetInitHandler(common.AFIP, handler.SuccessHandler)
 
 	// Meta
-	r.SetHandler("GetVersion", Free, handler.ValueHandler(Version))
-	r.SetHandler("GetFunctions", Free, router.FunctionsHandler(r))
+	r.SetHandler("GetVersion", common.Free, handler.ValueHandler(Version))
+	r.SetHandler("GetFunctions", common.Free, router.FunctionsHandler(r))
 
 	// Business
-	r.SetHandler("GetPersona", Free, persona.GetPersonaHandler)
-	r.SetHandler("DelPersona", OnlyAFIP, persona.DelPersonaHandler)
-	r.SetHandler("PutPersona", OnlyAFIP, persona.PutPersonaHandler)
-	r.SetHandler("PutPersonaList", OnlyAFIP, persona.PutPersonaListHandler)
-
-	// Business (debugging)
-	r.SetHandler("GetPersonaRange", Free, persona.GetPersonaRangeHandler)
-	r.SetHandler("DelPersonaRange", OnlyAFIP, persona.DelPersonaRangeHandler)
+	persona.AddHandlers(r)
+	impuesto.AddHandlers(r)
 	r.SetHandler("GetPersonaAll", Free, persona.GetPersonaAllHandler)
 
-	// Generic
-	r.SetHandler("GetStates", Free, generic.GetStatesHandler)
-	r.SetHandler("DelStates", OnlyAFIP, generic.DelStatesHandler)
-	r.SetHandler("PutStates", OnlyAFIP, generic.PutStatesHandler)
-
-	// History
-	r.SetHandler("GetStatesHistory", Free, generic.GetStatesHistoryHandler)
+	// States
+	state.AddHandlers(r)
 
 	// ======================== Deprecated handlers ============================
 	//
@@ -59,18 +44,18 @@ func main() {
 	// no presentan al cliente una interfaz robusta, consistente y homogenea.
 	//
 	// TODO eliminar bloque antes de 1.0.0
-	r.SetHandler("putPersona", OnlyAFIP, deprecated.Adapter(personas.PutPersona, "PutPersona"))
-	r.SetHandler("delPersona", OnlyAFIP, deprecated.Adapter(personas.DelPersona, "DelPersona"))
+	r.SetHandler("putPersona", common.AFIP, deprecated.Adapter(personas.PutPersona, "PutPersona"))
+	r.SetHandler("delPersona", common.AFIP, deprecated.Adapter(personas.DelPersona, "DelPersona"))
 	r.SetHandler("getPersona", Free, deprecated.Adapter(personas.GetPersonaAPI, "GetPersona"))
-	r.SetHandler("putPersonas", OnlyAFIP, deprecated.Adapter(personas.PutPersonas, "PutPersonaList"))
-	r.SetHandler("delPersonasByRange", OnlyAFIP, deprecated.Adapter(personas.DelPersonasByRange, "DelPersonaRange"))
-	r.SetHandler("deleteAll", OnlyAFIP, deprecated.Adapter(fabric.DeleteAll, "DelStates"))
-	r.SetHandler("deleteByKeyRange", OnlyAFIP, deprecated.Adapter(fabric.DeleteByKeyRange, "DelStates"))
-	r.SetHandler("queryPersona", OnlyAFIP, deprecated.Adapter(personas.QueryPersona, "GetStates"))
-	r.SetHandler("queryAllPersona", OnlyAFIP, deprecated.Adapter(personas.QueryAllPersona, "GetStates"))
-	r.SetHandler("queryHistory", OnlyAFIP, deprecated.Adapter(fabric.QueryHistory, "GetStatesHistory"))
-	r.SetHandler("queryByKey", OnlyAFIP, deprecated.Adapter(fabric.QueryByKey, "GetStates"))
-	r.SetHandler("queryByKeyRange", OnlyAFIP, deprecated.Adapter(fabric.QueryByKeyRange, "GetStates"))
+	r.SetHandler("putPersonas", common.AFIP, deprecated.Adapter(personas.PutPersonas, "PutPersonaList"))
+	r.SetHandler("delPersonasByRange", common.AFIP, deprecated.Adapter(personas.DelPersonasByRange, "DelPersonaRange"))
+	r.SetHandler("deleteAll", common.AFIP, deprecated.Adapter(fabric.DeleteAll, "DelStates"))
+	r.SetHandler("deleteByKeyRange", common.AFIP, deprecated.Adapter(fabric.DeleteByKeyRange, "DelStates"))
+	r.SetHandler("queryPersona", common.AFIP, deprecated.Adapter(personas.QueryPersona, "GetStates"))
+	r.SetHandler("queryAllPersona", common.AFIP, deprecated.Adapter(personas.QueryAllPersona, "GetStates"))
+	r.SetHandler("queryHistory", common.AFIP, deprecated.Adapter(fabric.QueryHistory, "GetStatesHistory"))
+	r.SetHandler("queryByKey", common.AFIP, deprecated.Adapter(fabric.QueryByKey, "GetStates"))
+	r.SetHandler("queryByKeyRange", common.AFIP, deprecated.Adapter(fabric.QueryByKeyRange, "GetStates"))
 	//
 	// =========================================================================
 
