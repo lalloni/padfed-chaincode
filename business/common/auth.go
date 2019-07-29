def4562e895ce1c84806898cfac11f03921980ca
Pablo Lalloni <plalloni@gmail.com>
2019-07-29 19:16:56 -0300

    Introduce función para extraer CUIT del SerialNumber del Subject del certificado del cliente

--- business/common/auth.go
+++ business/common/auth.go
@@ -1,8 +1,33 @@
 package common
 
-import "github.com/lalloni/fabrikit/chaincode/authorization"
+import (
+	"regexp"
+	"strconv"
+
+	"github.com/lalloni/fabrikit/chaincode/authorization"
+	"github.com/lalloni/fabrikit/chaincode/context"
+	"github.com/pkg/errors"
+)
 
 var (
 	AFIP = authorization.MSPID("AFIP")
 	Free = authorization.Allowed
 )
+
+var cuitSerialNumberRegexp = regexp.MustCompile(`^CUIT (\d{11})$`)
+
+func ExtractCUITFromSerialNumber(ctx *context.Context) (uint64, error) {
+	cert, err := ctx.ClientCertificate()
+	if err != nil {
+		return 0, errors.Wrap(err, "getting client certificate")
+	}
+	match := cuitSerialNumberRegexp.FindStringSubmatch(cert.Subject.SerialNumber)
+	if match == nil {
+		return 0, errors.New("invalid cuit syntax in certificate subject serialnumber")
+	}
+	cuit, err := strconv.ParseUint(match[1], 10, 64)
+	if err != nil {
+		return 0, errors.Wrap(err, "invalid cuit in certificate subject serialnumber")
+	}
+	return cuit, nil
+}
